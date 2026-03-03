81928c2f06854f5d58712f91032c8f393b4c56ec
ApongsehIyan23 <apongsehiyan23@gmail.com>
2026-03-03 18:08:50 +0200

    Added 0-transfer_file.sh script

---
+++ web_server/0-transfer_file.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+# 0-transfer_file.sh
+#script that transfers file from local machine to a remote server using scp command
+
+if ["$#" -eq  3 || "$#" -eq 4]; then
+    echo "User entered 3 or 4 arguments"
+else
+    echo "Invalid number of arguments passed.. be sure to enter 3 or 4 arguments"
+    exit 1
+fi
